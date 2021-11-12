dd23e947628b4b70ba109fb5ff406d333dab9f86
Chris Cassano <chris@litprotocol.com>
2021-11-12 13:57:23 -0800

    got poap list loaded, but having trouble creating a searchable editable select box.  want user to be able to search for Burning Man 2021, find it, and delete the 2021 for example

--- dist/ShareModalSteps/ChoosePOAP.js
+++ dist/ShareModalSteps/ChoosePOAP.js
@@ -7,6 +7,10 @@ exports.default = void 0;
 
 require("core-js/modules/web.dom-collections.iterator.js");
 
+require("core-js/modules/es.promise.js");
+
+require("core-js/modules/es.object.assign.js");
+
 var _react = _interopRequireWildcard(require("react"));
 
 var _shareModalModule = _interopRequireDefault(require("../share-modal.module.scss"));
@@ -15,7 +19,11 @@ var _Button = require("@consta/uikit/Button");
 
 var _IconBackward = require("@consta/uikit/IconBackward");
 
-var _reactSelect = _interopRequireDefault(require("react-select"));
+var _ProgressSpin = require("@consta/uikit/ProgressSpin");
+
+var _reactSelect = require("react-select");
+
+var _reactSelectVirtualized = require("react-select-virtualized");
 
 var _InputWrapper = _interopRequireDefault(require("../InputWrapper/InputWrapper"));
 
@@ -24,6 +32,12 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+const Input = props => /*#__PURE__*/_react.default.createElement(_reactSelect.components.Input, _extends({}, props, {
+  isHidden: false
+}));
 
 const matchConditionOptions = [{
   label: "Equals POAP Name exactly",
@@ -35,13 +49,53 @@ const matchConditionOptions = [{
   value: "contains"
 }];
 
-const DAOMembers = _ref => {
+const ChoosePOAP = _ref => {
   let {
     setActiveStep,
     onAccessControlConditionsSelected
   } = _ref;
   const [POAPName, setPOAPName] = (0, _react.useState)("");
   const [matchCondition, setMatchCondition] = (0, _react.useState)(null);
+  const [poapList, setPoapList] = (0, _react.useState)([]);
+  const [selectedPoap, setSelectedPoap] = (0, _react.useState)(null);
+  const [inputValue, setInputValue] = (0, _react.useState)("");
+  const selectRef = (0, _react.useRef)();
+
+  const onInputChange = (inputValue, _ref2) => {
+    let {
+      action
+    } = _ref2;
+
+    // onBlur => setInputValue to last selected value
+    if (action === "input-blur") {
+      setInputValue(selectedPoap ? selectedPoap.label : "");
+    } // onInputChange => update inputValue
+    else if (action === "input-change") {
+      setInputValue(inputValue);
+    }
+  };
+
+  const onChange = option => {
+    setSelectedPoap(option);
+    setInputValue(option ? option.label : "");
+  };
+
+  const onFocus = () => selectedPoap && selectRef.current.select.inputRef.select();
+
+  (0, _react.useEffect)(() => {
+    const go = async () => {
+      const url = "https://api.poap.xyz/events";
+      const resp = await fetch(url).then(response => response.json());
+      console.log("resp", resp);
+      setPoapList(resp.map(r => ({
+        label: r.name,
+        id: r.name,
+        value: r.name
+      })));
+    };
+
+    go();
+  }, []);
 
   const handleSubmit = () => {
     const chain = "xdai";
@@ -78,7 +132,24 @@ const DAOMembers = _ref => {
     className: _shareModalModule.default.icon
   }), " Back"), /*#__PURE__*/_react.default.createElement("div", {
     className: _shareModalModule.default.titles
-  }, /*#__PURE__*/_react.default.createElement("h3", null, "Which POAP holders should be able to access this?")), /*#__PURE__*/_react.default.createElement("div", {
+  }, /*#__PURE__*/_react.default.createElement("h3", null, "Which POAP holders should be able to access this?")), !poapList || poapList.length == 0 ? /*#__PURE__*/_react.default.createElement(_ProgressSpin.ProgressSpin, null) : /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("div", {
+    className: _shareModalModule.default.form
+  }, /*#__PURE__*/_react.default.createElement(_reactSelectVirtualized.Select, {
+    isClearable: true,
+    defaultValue: "",
+    options: poapList,
+    value: selectedPoap,
+    isLoading: !poapList || poapList.length == 0,
+    ref: selectRef,
+    inputValue: inputValue,
+    onInputChange: onInputChange,
+    onChange: onChange,
+    onFocus: onFocus,
+    controlShouldRenderValue: false,
+    components: {
+      Input
+    }
+  })), /*#__PURE__*/_react.default.createElement("div", {
     className: _shareModalModule.default.form
   }, /*#__PURE__*/_react.default.createElement(_InputWrapper.default, {
     value: POAPName,
@@ -92,7 +163,7 @@ const DAOMembers = _ref => {
     className: _shareModalModule.default.select
   }, /*#__PURE__*/_react.default.createElement("span", {
     className: _shareModalModule.default.label
-  }, "Match Conditions"), /*#__PURE__*/_react.default.createElement(_reactSelect.default, {
+  }, "Match Conditions"), /*#__PURE__*/_react.default.createElement(_reactSelectVirtualized.Select, {
     isClearable: true,
     options: matchConditionOptions,
     value: matchCondition,
@@ -103,8 +174,8 @@ const DAOMembers = _ref => {
     size: "l",
     onClick: handleSubmit,
     disabled: !POAPName || !matchCondition
-  })));
+  }))));
 };
 
-var _default = DAOMembers;
+var _default = ChoosePOAP;
 exports.default = _default;
--- src/ShareModalSteps/ChoosePOAP.jsx
+++ src/ShareModalSteps/ChoosePOAP.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 import styles from "../share-modal.module.scss";
 
 import { Button } from "@consta/uikit/Button";
 import { IconBackward } from "@consta/uikit/IconBackward";
+import { ProgressSpin } from "@consta/uikit/ProgressSpin";
 
-import Select from "react-select";
+import { components } from "react-select";
+import { Select } from "react-select-virtualized";
 
 import InputWrapper from "../InputWrapper/InputWrapper";
+
+const Input = (props) => <components.Input {...props} isHidden={false} />;
 
 const matchConditionOptions = [
   {
@@ -22,9 +26,50 @@ const matchConditionOptions = [
   },
 ];
 
-const DAOMembers = ({ setActiveStep, onAccessControlConditionsSelected }) => {
+const ChoosePOAP = ({ setActiveStep, onAccessControlConditionsSelected }) => {
   const [POAPName, setPOAPName] = useState("");
   const [matchCondition, setMatchCondition] = useState(null);
+  const [poapList, setPoapList] = useState([]);
+  const [selectedPoap, setSelectedPoap] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+
+  const selectRef = useRef();
+
+  const onInputChange = (inputValue, { action }) => {
+    // onBlur => setInputValue to last selected value
+    if (action === "input-blur") {
+      setInputValue(selectedPoap ? selectedPoap.label : "");
+    }
+
+    // onInputChange => update inputValue
+    else if (action === "input-change") {
+      setInputValue(inputValue);
+    }
+  };
+
+  const onChange = (option) => {
+    setSelectedPoap(option);
+    setInputValue(option ? option.label : "");
+  };
+
+  const onFocus = () =>
+    selectedPoap && selectRef.current.select.inputRef.select();
+
+  useEffect(() => {
+    const go = async () => {
+      const url = "https://api.poap.xyz/events";
+      const resp = await fetch(url).then((response) => response.json());
+      console.log("resp", resp);
+      setPoapList(
+        resp.map((r) => ({
+          label: r.name,
+          id: r.name,
+          value: r.name,
+        }))
+      );
+    };
+    go();
+  }, []);
 
   const handleSubmit = () => {
     const chain = "xdai";
@@ -67,35 +112,60 @@ const DAOMembers = ({ setActiveStep, onAccessControlConditionsSelected }) => {
       <div className={styles.titles}>
         <h3>Which POAP holders should be able to access this?</h3>
       </div>
-      <div className={styles.form}>
-        <InputWrapper
-          value={POAPName}
-          className={styles.input}
-          label="POAP Name"
-          id="POAPName"
-          autoFocus
-          size="m"
-          handleChange={(value) => setPOAPName(value)}
-        />
-        <div className={styles.select}>
-          <span className={styles.label}>Match Conditions</span>
-          <Select
-            isClearable
-            options={matchConditionOptions}
-            value={matchCondition}
-            onChange={(value) => setMatchCondition(value)}
-          />
-        </div>
-        <Button
-          label="Create  Requirment"
-          className={styles.btn}
-          size="l"
-          onClick={handleSubmit}
-          disabled={!POAPName || !matchCondition}
-        />
-      </div>
+
+      {!poapList || poapList.length == 0 ? (
+        <ProgressSpin />
+      ) : (
+        <>
+          <div className={styles.form}>
+            <Select
+              isClearable
+              defaultValue=""
+              options={poapList}
+              value={selectedPoap}
+              isLoading={!poapList || poapList.length == 0}
+              ref={selectRef}
+              inputValue={inputValue}
+              onInputChange={onInputChange}
+              onChange={onChange}
+              onFocus={onFocus}
+              controlShouldRenderValue={false}
+              components={{
+                Input,
+              }}
+            />
+          </div>
+          <div className={styles.form}>
+            <InputWrapper
+              value={POAPName}
+              className={styles.input}
+              label="POAP Name"
+              id="POAPName"
+              autoFocus
+              size="m"
+              handleChange={(value) => setPOAPName(value)}
+            />
+            <div className={styles.select}>
+              <span className={styles.label}>Match Conditions</span>
+              <Select
+                isClearable
+                options={matchConditionOptions}
+                value={matchCondition}
+                onChange={(value) => setMatchCondition(value)}
+              />
+            </div>
+            <Button
+              label="Create  Requirment"
+              className={styles.btn}
+              size="l"
+              onClick={handleSubmit}
+              disabled={!POAPName || !matchCondition}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 };
 
-export default DAOMembers;
+export default ChoosePOAP;
