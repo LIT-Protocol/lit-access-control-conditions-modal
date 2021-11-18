cb144b6da50ee9865c2c5f9479aece4284529a0a
Nick <forizual@gmail.com>
2021-11-18 17:57:43 +0300

    update choose type step

---
+++ src/Modal/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import cx from 'classnames'
+
+import styles from './modal.module.scss'
+
+import { Modal } from '@consta/uikit/Modal'
+import { IconClose } from "@consta/uikit/IconClose";
+
+const ModalComponent = (props) => {
+  const {
+    isOpen,
+    children,
+    title,
+    withCloseButton = true,
+    unsavedPopup = false,
+    onClose = () => false,
+  } = props
+
+  const passedProps = { ...props }
+  delete passedProps.children
+  delete passedProps.withCloseButton
+  delete passedProps.onClose
+  delete passedProps.unsavedPopup
+  delete passedProps.title
+
+  const handleClose = () => {
+    onClose()
+  }
+
+  if (!isOpen) {
+    return null
+  }
+
+  return (
+    <Modal
+      {...passedProps}
+      onOverlayClick={handleClose}
+      className={cx(passedProps.className, styles.modal)}
+    >
+      {withCloseButton ? (
+        <div className={styles.closeButton}>
+          <IconClose className={styles.icon} onClick={handleClose} />
+        </div>
+      ) : null}
+      <div>
+        {title ? <div className={styles.title}>{title}</div> : null}
+        {children}
+      </div>
+    </Modal>
+  )
+}
+
+export default ModalComponent
