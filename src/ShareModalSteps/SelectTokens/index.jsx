9f8214e660c1b599f9ce0780c4cb697fd36fa8d2
Chris Cassano <chris@litprotocol.com>
2021-11-23 17:45:07 -0500

    Add corejs

--- src/ShareModalSteps/SelectTokens/index.jsx
+++ src/ShareModalSteps/SelectTokens/index.jsx
@@ -1,127 +1,127 @@
-import React, { useState } from 'react'
-import { ethers } from 'ethers'
-import LitJsSdk from 'lit-js-sdk'
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import LitJsSdk from "lit-js-sdk";
 
-import styles from './select-tokens.module.scss'
+import styles from "./select-tokens.module.scss";
 
-import InputWrapper from '../../InputWrapper/InputWrapper'
-import ChainSelector from '../../ChainSelector/ChainSelector'
-import Navigation from '../../Navigation';
-import TokenSelect from '../../TokenSelect';
+import InputWrapper from "../../InputWrapper/InputWrapper";
+import ChainSelector from "../../ChainSelector/ChainSelector";
+import Navigation from "../../Navigation";
+import TokenSelect from "../../TokenSelect";
 
-
-const SelectTokens = ({ setActiveStep, onAccessControlConditionsSelected, tokenList }) => {
-  const [amount, setAmount] = useState('')
-  const [selectedToken, setSelectedToken] = useState(null)
-  const [contractAddress, setContractAddress] = useState('')
-  const [chain, setChain] = useState(null)
+const SelectTokens = ({
+  setActiveStep,
+  onAccessControlConditionsSelected,
+  tokenList,
+}) => {
+  const [amount, setAmount] = useState("");
+  const [selectedToken, setSelectedToken] = useState(null);
+  const [contractAddress, setContractAddress] = useState("");
+  const [chain, setChain] = useState(null);
 
   const handleSubmit = async () => {
-    console.log('handleSubmit and selectedToken is', selectedToken)
+    console.log("handleSubmit and selectedToken is", selectedToken);
 
-    if (selectedToken.value === 'ethereum') {
+    if (selectedToken.value === "ethereum") {
       // ethereum
-      const amountInWei = ethers.utils.parseEther(amount)
+      const amountInWei = ethers.utils.parseEther(amount);
       const accessControlConditions = [
         {
-          contractAddress: '',
-          standardContractType: '',
+          contractAddress: "",
+          standardContractType: "",
           chain: chain.value,
-          method: 'eth_getBalance',
-          parameters: [
-            ':userAddress',
-            'latest'
-          ],
+          method: "eth_getBalance",
+          parameters: [":userAddress", "latest"],
           returnValueTest: {
-            comparator: '>=',
-            value: amountInWei.toString()
-          }
-        }
-      ]
-      onAccessControlConditionsSelected(accessControlConditions)
+            comparator: ">=",
+            value: amountInWei.toString(),
+          },
+        },
+      ];
+      onAccessControlConditionsSelected(accessControlConditions);
     } else {
-      console.log('selectedToken', selectedToken)
+      console.log("selectedToken", selectedToken);
 
-      let tokenType
-      if (selectedToken.standard?.toLowerCase() === 'erc721') {
-        tokenType = 'erc721'
+      let tokenType;
+      if (selectedToken.standard?.toLowerCase() === "erc721") {
+        tokenType = "erc721";
       } else if (selectedToken.decimals) {
-        tokenType = 'erc20'
+        tokenType = "erc20";
       } else {
         // if we don't already know the type, try and get decimal places.  if we get back 0 or the request fails then it's probably erc721.
-        const authSig = await LitJsSdk.checkAndSignAuthMessage({ chain: chain.value })
-        let decimals = 0
+        let decimals = 0;
         try {
-          decimals = await LitJsSdk.decimalPlaces({ contractAddress: selectedToken.value })
+          decimals = await LitJsSdk.decimalPlaces({
+            contractAddress: selectedToken.value,
+          });
         } catch (e) {
-          console.log(e)
+          console.log(e);
         }
         if (decimals == 0) {
-          tokenType = 'erc721'
+          tokenType = "erc721";
         } else {
-          tokenType = 'erc20'
+          tokenType = "erc20";
         }
       }
 
-      console.log('tokenType is', tokenType)
+      console.log("tokenType is", tokenType);
 
-      if (tokenType == 'erc721') {
+      if (tokenType == "erc721") {
         // erc721
         const accessControlConditions = [
           {
             contractAddress: selectedToken.value,
-            standardContractType: 'ERC721',
+            standardContractType: "ERC721",
             chain: chain.value,
-            method: 'balanceOf',
-            parameters: [
-              ':userAddress'
-            ],
+            method: "balanceOf",
+            parameters: [":userAddress"],
             returnValueTest: {
-              comparator: '>',
-              value: '0'
-            }
-          }
-        ]
-        onAccessControlConditionsSelected(accessControlConditions)
+              comparator: ">=",
+              value: amount.toString(),
+            },
+          },
+        ];
+        onAccessControlConditionsSelected(accessControlConditions);
       } else {
         // erc20 token
-        let amountInBaseUnit
+        let amountInBaseUnit;
         if (selectedToken.decimals) {
-          amountInBaseUnit = ethers.utils.parseUnits(amount, selectedToken.decimals)
+          amountInBaseUnit = ethers.utils.parseUnits(
+            amount,
+            selectedToken.decimals
+          );
         } else {
           // need to check the contract for decimals
           // this will auto switch the chain to the selected one in metamask
-          const authSig = await LitJsSdk.checkAndSignAuthMessage({ chain: chain.value })
-          let decimals = 0
+          let decimals = 0;
           try {
-            decimals = await LitJsSdk.decimalPlaces({ contractAddress: selectedToken.value })
+            decimals = await LitJsSdk.decimalPlaces({
+              contractAddress: selectedToken.value,
+            });
           } catch (e) {
-            console.log(e)
+            console.log(e);
           }
-          console.log(`decimals in ${selectedToken.value}`, decimals)
-          amountInBaseUnit = ethers.utils.parseUnits(amount, decimals)
+          console.log(`decimals in ${selectedToken.value}`, decimals);
+          amountInBaseUnit = ethers.utils.parseUnits(amount, decimals);
         }
         const accessControlConditions = [
           {
             contractAddress: selectedToken.value,
-            standardContractType: 'ERC20',
+            standardContractType: "ERC20",
             chain: chain.value,
-            method: 'balanceOf',
-            parameters: [
-              ':userAddress'
-            ],
+            method: "balanceOf",
+            parameters: [":userAddress"],
             returnValueTest: {
-              comparator: '>=',
-              value: amountInBaseUnit.toString()
-            }
-          }
-        ]
-        onAccessControlConditionsSelected(accessControlConditions)
+              comparator: ">=",
+              value: amountInBaseUnit.toString(),
+            },
+          },
+        ];
+        onAccessControlConditionsSelected(accessControlConditions);
       }
     }
-    setActiveStep('accessCreated')
-
-  }
+    setActiveStep("accessCreated");
+  };
 
   // const formatOptionLabel = (option, extra) => {
   //   const { name, logoURI, value } = option
@@ -156,54 +156,51 @@ const SelectTokens = ({ setActiveStep, onAccessControlConditionsSelected, tokenL
       </div>
       <div className={styles.form}>
         <div className={styles.inputMaxWidth}>
-            <div className={styles.select}>
+          <div className={styles.select}>
             <label>Select blockchain to check requirements against:</label>
             <ChainSelector chain={chain} setChain={setChain} />
-            </div>
+          </div>
         </div>
         <div className={styles.select}>
-          <label>Select token/NFT or enter contract address:  </label>
+          <label>Select token/NFT or enter contract address: </label>
           <div className={styles.tokenOrContractAddress}>
-            <TokenSelect 
-                tokenList={tokenList} 
-                onSelect={setSelectedToken}
-            />
+            <TokenSelect tokenList={tokenList} onSelect={setSelectedToken} />
             <div className={styles.separator}>OR</div>
             <InputWrapper
-                placeholder="ERC20 or ERC721 address"
-                value={contractAddress}
-                className={styles.input}
-                id="amount"
-                autoFocus
-                size="m"
-                handleChange={setContractAddress}
+              placeholder="ERC20 or ERC721 address"
+              value={contractAddress}
+              className={styles.input}
+              id="amount"
+              autoFocus
+              size="m"
+              handleChange={setContractAddress}
             />
           </div>
         </div>
         <div className={styles.inputMaxWidth}>
-            <InputWrapper
-                value={amount}
-                className={styles.input}
-                label="How many tokens does the wallet need to own?"
-                id="amount"
-                autoFocus
-                size="m"
-                handleChange={(value) => setAmount(value)}
-            />
+          <InputWrapper
+            value={amount}
+            className={styles.input}
+            label="How many tokens does the wallet need to own?"
+            id="amount"
+            autoFocus
+            size="m"
+            handleChange={(value) => setAmount(value)}
+          />
         </div>
       </div>
 
-      <Navigation 
-            backward={{ onClick: () => setActiveStep("ableToAccess") }} 
-            forward={{ 
-                label: 'Create Requirment', 
-                onClick: handleSubmit, 
-                withoutIcon: true,
-                disabled: !amount || !(selectedToken  || contractAddress) || !chain
-            }}
-       />
+      <Navigation
+        backward={{ onClick: () => setActiveStep("ableToAccess") }}
+        forward={{
+          label: "Create Requirment",
+          onClick: handleSubmit,
+          withoutIcon: true,
+          disabled: !amount || !(selectedToken || contractAddress) || !chain,
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default SelectTokens
+export default SelectTokens;
