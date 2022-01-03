684fb18c3e2304bbd6d6e044627e8c28641dfaee
Chris Cassano <chris@litprotocol.com>
2022-01-03 18:51:41 -0500

    fix litgate

--- src/TokenSelect/index.jsx
+++ src/TokenSelect/index.jsx
@@ -42,14 +42,14 @@ const TOP_LIST = [
   {
     label: "Lit Genesis Gate",
     logo: "https://litgateway.com/favicon.png",
-    address: "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a",
+    value: "0xA3D109E28589D2AbC15991B57Ce5ca461Ad8e026",
     symbol: "LITGATE",
     standard: "ERC721",
   },
   {
     label: "Art Blocks",
     logo: "https://lh3.googleusercontent.com/sdPql8yt3eT5qmQfbCoU8a1I6aMNsqQEj6D1fMTuw101XKILNmzp7QVsdkGff2T39MgcHT-Aha18cWBqjCdhzRWzBw=s120",
-    address: "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a",
+    value: "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a",
     symbol: "BLOCKS",
     standard: "ERC721",
   },
